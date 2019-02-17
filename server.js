12c85ee1b73efa728b0d9eeae18c8dee956ae556
lisu9090 <lisu9090@gmail.com>
2019-02-17 11:42:37 +0100

    Static files

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ const YD = new YoutubeMp3Downloader({
 app.listen(port, () => console.log(`YouTube mp3 downloader is listening on port ${port}!`));
 
 app.get('/', (req, res) => {
-    // res.sendFile('/views/index.html')
-    res.send('test');
+    res.sendFile(path.join(__dirname + '/views/index.html'))
 });
 
 app.get('/getaudio/:vid', (req, res) => {
@@ -56,9 +55,10 @@ app.get('/getaudio/:vid', (req, res) => {
 
 });
 
-app.post('/cancel/:vid', (req, res) => {
-    //YD...
-});
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/scripts'));
+
+app.use('/', router);
 
 const removeFile = function(fileName){
     if(fileName){
